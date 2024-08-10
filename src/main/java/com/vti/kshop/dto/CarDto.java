689b8107ffe7bd7f3b38066b525de677ac0e0204
aginCoder <phamgiaan545@gmail.com>
2024-08-10 12:30:12 +0700

    kshop

--- src/main/java/com/vti/kshop/dto/CarDto.java
+++ src/main/java/com/vti/kshop/dto/CarDto.java
@@ -1,13 +1,18 @@
 package com.vti.kshop.dto;
 
+import com.vti.kshop.controller.CarController;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
 
 import java.time.LocalDate;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @Getter
 @Setter
-public class CarDto {
+public class CarDto extends RepresentationModel<CarDto>{
     private Long id;
     private String licensePlate;
     private LocalDate repairDate;
@@ -15,5 +20,10 @@ public class CarDto {
     private String catalog;
     private String carMaker;
 
-
+    public CarDto withSelfRel() {
+        var controller = methodOn(CarController.class);
+        var dto = controller.findById(id);
+        var link = linkTo(dto).withSelfRel();
+        return add(link);
+    }
 }
