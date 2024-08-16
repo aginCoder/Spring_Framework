7c1e7ed638d2f49196a9d18a70d7217403d3f243
aginCoder <phamgiaan545@gmail.com>
2024-08-16 21:26:19 +0700

    blog-app

--- src/main/java/com/vti/springframework/configuration/SecurityConfiguration.java
+++ src/main/java/com/vti/springframework/configuration/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.vti.springframework.configuration;
 
 import com.vti.springframework.entity.User;
+import com.vti.springframework.exception.ErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -14,7 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(
-            HttpSecurity http
+            HttpSecurity http, ErrorHandler errorHandler
     ) throws Exception {
         http
                 .csrf(customizer -> customizer.disable())
@@ -22,6 +23,9 @@ public class SecurityConfiguration {
                 .httpBasic(customizer -> {})
                 .sessionManagement(customizer -> customizer
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(customiez -> customiez
+                        .authenticationEntryPoint(errorHandler)
+                        .accessDeniedHandler(errorHandler))
                 .authorizeHttpRequests(customizer -> customizer
                         .requestMatchers(HttpMethod.DELETE)
                         .hasAuthority(User.Role.ADMIN.name())
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,8 @@ INSERT INTO post (id, title            , content                           , des
 VALUES           (1 ,'Spring Framework', 'Learn Spring Framework with Khoa', 'Learn Spring Framework', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO comment (id, name             , email              , body              , post_id, created_at       , updated_at       )
-VALUES              (1 , 'Phạm Gia Ân', 'an,pg@gmail.com', 'Bài viết rất hay', 1      , CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES              (1 , 'Phạm Gia Ân', 'an.pg@gmail.com', 'Bài viết rất hay', 1      , CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+--INSERT INTO users (name, email, username, password, role, created_at, updated_at)
+--VALUES ('Thu Uyn', 'uyn.ptt@gmail.com', 'uyn.ptt', '123456A', 'EMPLOYEE', NOW(), NOW()),
+--        ('Agin', 'angin@gmail.com', 'agin', '123456A', 'EMPLOYEE', NOW(), NOW());
